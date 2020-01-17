e38050ba344de10184bd4a3f7c67f94653c146d7
Lambda_School_Loaner_252 <c0der.br1t@gmail.com>
2020-01-17 12:40:55 -0600

    created a few more styling rules in css. Created a test for App.js. Attempted another test. Answered Readme questions.

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,10 @@ class App extends React.Component {
   render() {
   return (
     <div className="App">
-      <Mode />
+      <div data-testid="mode-btn">
+        <Mode /> 
+      </div>
+      
       <header className="App-header">
         <h1>Women's World Cup Players</h1>
         <h3>The following women are the Women's World Cup players, ranked by search interest from Google Trends from June-July 2019, worldwide.</h3>
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,18 @@ import React from "react";
 import { render } from '@testing-library/react';
 import App from './App';
 
-TextDecoderStream("App renders without crashing.", () => {
-  const component = render(<App />);
+test("App renders without crashing.", () => {
+  render(<App />);
 });
+
+// test("Contains the phrase 'Google Trends'.", () => {
+//   const wrap = render(<App />);
+//   const hasText = wrap.queryByText(/google trends/i);
+//   expect(hasText).toBeInTheDocument();
+// })
+
+test("Contains a mode button", () => {
+  const { getByTestId } = render(<App />);
+
+  getByTestId(/mode-btn/i);
+})
